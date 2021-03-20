6268464c41fbef04c3a556f2d9b55b9d78e2d042
Danilo Caetano <danilo.lopz@gmail.com>
2021-03-20 16:52:10 -0300

    update js 20032021

---
+++ js/custom.js
@@ -0,0 +1,31 @@
+// add new morador
+
+$(document).ready(function() {
+
+  $("#qtd_moradores").change( function() {
+
+    let qtd = 1;
+    const qtdValue = $("#qtd_moradores option:selected").val();
+
+    const original = document.getElementById('row-morador');
+    const copy = document.getElementById('mais-moradores');
+
+    var newSelect = original.cloneNode(true);
+
+    while(qtd < qtdValue) {
+      copy.appendChild(newSelect);
+      qtd++;
+    }
+
+  });
+
+  $("#add-comprovante").click( function() {
+
+    const original = document.getElementById('row-comprovante');
+    var newSelect = original.cloneNode(true);
+
+    original.after(newSelect);
+
+  });
+
+});
